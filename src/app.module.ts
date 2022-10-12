c5fd8bd3510f92d9b82adbfd5e8b44b4b1a31369
krutulkamil <krutulkamil@onet.pl>
2022-10-12 20:45:02 +0200

    feat: connect to local mongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RestaurantsModule } from './restaurants/restaurants.module';
 
 @Module({
-  imports: [],
+  imports: [
+      ConfigModule.forRoot({
+          envFilePath: `.env.${process.env.NODE_ENV || 'development'}`,
+          isGlobal: true
+      }),
+      MongooseModule.forRoot(process.env.DB_URI_LOCAL),
+      RestaurantsModule],
   controllers: [AppController],
   providers: [AppService],
 })
