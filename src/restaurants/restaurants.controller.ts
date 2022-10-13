1db587e525e0991eca3d0470f92cb5a27d1d9596
krutulkamil <krutulkamil@onet.pl>
2022-10-13 19:44:34 +0200

    feat: simple getAll/byId & create requests

--- src/restaurants/restaurants.controller.ts
+++ src/restaurants/restaurants.controller.ts
@@ -1,7 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { RestaurantsService } from './restaurants.service';
+import { Restaurant } from './schemas/restaurant.schema';
+import { CreateRestaurantDto } from './dto/create-restaurant.dto';
 
 @Controller('restaurants')
 export class RestaurantsController {
-    constructor(private readonly restaurantsService: RestaurantsService) {}
+    constructor(private readonly restaurantsService: RestaurantsService) {};
+
+    @Get()
+    async getAllRestaurants(): Promise<Restaurant[]> {
+        return await this.restaurantsService.findAll();
+    };
+
+    @Post()
+    async createRestaurant(@Body() restaurantDto: CreateRestaurantDto): Promise<Restaurant> {
+        return await this.restaurantsService.create(restaurantDto);
+    };
+
+    @Get(':id')
+    async getRestaurant(@Param('id') id: string): Promise<Restaurant> {
+        return await this.restaurantsService.findById(id);
+    }
 }
--- src/restaurants/restaurants.service.ts
+++ src/restaurants/restaurants.service.ts
@@ -1,4 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import * as mongoose from 'mongoose';
+import { Restaurant } from './schemas/restaurant.schema';
+import { CreateRestaurantDto } from './dto/create-restaurant.dto';
 
 @Injectable()
-export class RestaurantsService {}
+export class RestaurantsService {
+    constructor(
+        @InjectModel(Restaurant.name)
+        private readonly restaurantModel: mongoose.Model<Restaurant>
+    ) {}
+
+    async findAll(): Promise<Restaurant[]> {
+        const restaurants = await this.restaurantModel.find();
+        return restaurants;
+    };
+
+    async create(restaurantDto: CreateRestaurantDto): Promise<Restaurant> {
+        const restaurant = await this.restaurantModel.create(restaurantDto);
+        return restaurant;
+    };
+
+    async findById(id: string): Promise<Restaurant> {
+        const restaurant = await this.restaurantModel.findById(id);
+
+        if (!restaurant) {
+            throw new NotFoundException('Restaurant not found.');
+        }
+
+        return restaurant;
+    }
+}
