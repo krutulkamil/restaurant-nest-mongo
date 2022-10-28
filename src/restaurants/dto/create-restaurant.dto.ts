84accdd54adeff7bd16cc9614caa7d885c168cc8
krutulkamil <krutulkamil@onet.pl>
2022-10-28 11:05:27 +0200

    feat: add validation to create/update restaurant

--- src/restaurants/dto/create-restaurant.dto.ts
+++ src/restaurants/dto/create-restaurant.dto.ts
@@ -1,10 +1,28 @@
+import { IsEmail, IsEnum, IsNotEmpty, IsPhoneNumber, IsString } from 'class-validator';
 import { Category } from '../schemas/restaurant.schema';
 
 export class CreateRestaurantDto {
+    @IsNotEmpty({ message: 'Name field should not be empty' })
+    @IsString({ message: 'Name must be a string!' })
     readonly name: string;
+
+    @IsNotEmpty({ message: 'Description field should not be empty' })
+    @IsString({ message: 'Description must be a string!' })
     readonly description: string;
+
+    @IsNotEmpty({ message: 'Email field should not be empty' })
+    @IsEmail({}, { message: 'Please enter correct email address' })
     readonly email: string;
+
+    @IsNotEmpty({ message: 'Phone number field should not be empty' })
+    @IsPhoneNumber('PL', { message: 'Please enter correct phone number' })
     readonly phoneNo: number;
+
+    @IsNotEmpty({ message: 'Address field should not be empty' })
+    @IsString({ message: 'Address is required!' })
     readonly address: string;
+
+    @IsNotEmpty({ message: 'Category field should not be empty' })
+    @IsEnum(Category, { message: 'Please enter correct category' })
     readonly category: Category;
 }
--- src/restaurants/dto/update-restaurant.dto.ts
+++ src/restaurants/dto/update-restaurant.dto.ts
@@ -1,10 +1,28 @@
 import { Category } from '../schemas/restaurant.schema';
+import { IsEmail, IsEnum, IsOptional, IsPhoneNumber, IsString } from 'class-validator';
 
 export class UpdateRestaurantDto {
+    @IsString({ message: 'Name must be a string!' })
+    @IsOptional()
     readonly name: string;
+
+    @IsString({ message: 'Description must be a string!' })
+    @IsOptional()
     readonly description: string;
+
+    @IsEmail({}, { message: 'Please enter correct email address' })
+    @IsOptional()
     readonly email: string;
+
+    @IsPhoneNumber('PL', { message: 'Please enter correct phone number' })
+    @IsOptional()
     readonly phoneNo: number;
+
+    @IsString({ message: 'Address is required!' })
+    @IsOptional()
     readonly address: string;
+
+    @IsEnum(Category, { message: 'Please enter correct category' })
+    @IsOptional()
     readonly category: Category;
 }
