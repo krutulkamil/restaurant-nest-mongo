e785993ae06101c0e4f991499ecc21b584a8ec58
krutulkamil <krutulkamil@onet.pl>
2022-10-30 11:13:39 +0100

    feat: auth signup controller/service

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { SignUpDto } from './dto/signup.dto';
+import { User } from './schemas/user.schema';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+    constructor(private readonly authService: AuthService) {}
+
+    @Post('/signup')
+    async signUp(@Body() signUpDto: SignUpDto): Promise<User> {
+        return this.authService.signUp(signUpDto);
+    };
 }
