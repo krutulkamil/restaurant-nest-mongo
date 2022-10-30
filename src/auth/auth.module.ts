5a4a9bcf3b6f972c3e0418972d90b1fe8c5b3f66
krutulkamil <krutulkamil@onet.pl>
2022-10-30 12:22:37 +0100

    feat: setup passport & jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,14 +1,27 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UserSchema } from './schemas/user.schema';
 
 @Module({
     imports: [
-        MongooseModule.forFeature([
-            { name: 'User', schema: UserSchema }
-        ])
+        PassportModule.register({ defaultStrategy: 'jwt' }),
+        JwtModule.registerAsync({
+            inject: [ConfigService],
+            useFactory: (config: ConfigService) => {
+                return {
+                    secret: config.get<string>('JWT_SECRET'),
+                    signOptions: {
+                        expiresIn: config.get<string | number>('JWT_EXPIRES')
+                    }
+                }
+            }
+        }),
+        MongooseModule.forFeature([{ name: 'User', schema: UserSchema }])
     ],
     controllers: [AuthController],
     providers: [AuthService]
