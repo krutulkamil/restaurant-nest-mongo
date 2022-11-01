4b67fa2fdf45bf592828af27fb385d700c5821fa
krutulkamil <krutulkamil@onet.pl>
2022-11-01 16:48:19 +0100

    test: restaurantController - getAllRestaurants

--- src/restaurants/restaurants.controller.spec.ts
+++ src/restaurants/restaurants.controller.spec.ts
@@ -1,9 +1,46 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { PassportModule } from '@nestjs/passport';
 import { RestaurantsController } from './restaurants.controller';
 import { RestaurantsService } from './restaurants.service';
-import { PassportModule } from '@nestjs/passport';
+import { UserRoles } from '../auth/schemas/user.schema';
 
-const mockRestaurantService = {};
+const mockRestaurant = {
+    _id: '635fa337ab4946e81213a56c',
+    name: 'Vera Napoli',
+    description: 'Italian pizza',
+    email: 'vera.n@gmail.com',
+    phoneNo: 123456789,
+    address: 'Warszawska 23, Katowice, Poland',
+    category: 'Fine Dinning',
+    images: [],
+    location: {
+        type: 'Point',
+        coordinates: [
+            19.0277778,
+            50.2572222
+        ],
+        formattedAddress: 'Warszawska 23, 40-014 Katowice, Poland',
+        country: 'Poland',
+        city: 'Katowice',
+        streetName: 'Warszawska',
+        streetNumber: '23',
+        zipcode: '40-014'
+    },
+    menu: [],
+    user: '635e59634f714fa8196511a0',
+    __v: 4
+};
+
+const mockUser = {
+    _id: '635e59634f714fa8196511a0',
+    email: 'krutulkamil@onet.pl',
+    name: 'krutulkamil',
+    role: UserRoles.USER
+};
+
+const mockRestaurantService = {
+    findAll: jest.fn().mockResolvedValueOnce([mockRestaurant])
+};
 
 describe('RestaurantsController', () => {
     let controller: RestaurantsController;
@@ -27,5 +64,14 @@ describe('RestaurantsController', () => {
 
     it('should be defined', () => {
         expect(controller).toBeDefined();
+    });
+
+    describe('getAllRestaurants', () => {
+        it('should get all restaurants', async () => {
+            const result = await controller.getAllRestaurants({ keyword: 'restaurant'});
+
+            expect(service.findAll).toHaveBeenCalled();
+            expect(result).toEqual([mockRestaurant]);
+        });
     });
 });
