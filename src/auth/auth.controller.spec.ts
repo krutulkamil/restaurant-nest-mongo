fdc9eee70a3b696c23c01f8934415c8ede2ff784
krutulkamil <krutulkamil@onet.pl>
2022-11-02 19:06:30 +0100

    test: authController - signUp

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -2,7 +2,12 @@ import { TestingModule, Test } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
-const mockAuthService = {};
+const jwtToken = 'jwtToken';
+
+const mockAuthService = {
+    signUp: jest.fn().mockResolvedValueOnce(jwtToken),
+    login: jest.fn().mockResolvedValueOnce(jwtToken)
+};
 
 describe('AuthController', () => {
     let controller: AuthController;
@@ -25,5 +30,20 @@ describe('AuthController', () => {
 
     it('should be defined', () => {
         expect(controller).toBeDefined();
+    });
+
+    describe('signUp', () => {
+        it('should register a new user', async () => {
+            const signUpDto = {
+                name: 'krutulkamil',
+                email: 'krutulkamil@onet.pl',
+                password: 'test12345'
+            };
+
+            const result = await controller.signUp(signUpDto);
+
+            expect(service.signUp).toHaveBeenCalled();
+            expect(result).toEqual(jwtToken);
+        });
     });
 });
