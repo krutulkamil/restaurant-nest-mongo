9c3f7a20f09803cc47a0de4dbfd0e039405d2954
krutulkamil <krutulkamil@onet.pl>
2022-11-02 18:27:38 +0100

    test: authService - signUp

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,11 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { getModelToken } from '@nestjs/mongoose';
 import { JwtModule } from '@nestjs/jwt';
+import { ConflictException } from '@nestjs/common';
 import { Model } from 'mongoose';
+import * as bcrypt from 'bcryptjs';
 import { AuthService } from './auth.service';
-import { User } from './schemas/user.schema';
+import { User, UserRoles } from './schemas/user.schema';
+import APIFeatures from '../utils/apiFeatures.utils';
 
-const mockAuthService = {};
+const mockUser = {
+    _id: '635e59634f714fa8196511a0',
+    email: 'krutulkamil@onet.pl',
+    name: 'krutulkamil',
+    role: UserRoles.USER,
+    password: 'hashedPassword'
+};
+
+const token = 'jwtToken';
+
+const mockAuthService = {
+    create: jest.fn()
+};
 
 describe('AuthService', () => {
     let service: AuthService;
@@ -34,5 +49,37 @@ describe('AuthService', () => {
 
     it('should be defined', () => {
         expect(service).toBeDefined();
+    });
+
+    describe('signup', () => {
+
+        const signUpDto = {
+            name: 'krutulkamil',
+            email: 'krutulkamil@onet.pl',
+            password: 'test12345'
+        };
+
+        it('should register a new user', async () => {
+            jest.spyOn(bcrypt, 'hash')
+                .mockResolvedValueOnce('testHash');
+
+            jest.spyOn(model, 'create')
+                .mockImplementationOnce(() => Promise.resolve(mockUser));
+
+            jest.spyOn(APIFeatures, 'assignJwtToken')
+                .mockResolvedValueOnce(token);
+
+            const result = await service.signUp(signUpDto);
+
+            expect(bcrypt.hash).toHaveBeenCalled();
+            expect(result.token).toEqual(token);
+        });
+
+        it('should throw duplicate email entered', async () => {
+            jest.spyOn(model, 'create')
+                .mockImplementationOnce(() => Promise.reject({ code: 11000 }));
+
+            await expect(service.signUp(signUpDto)).rejects.toThrow(ConflictException);
+        });
     });
 });
